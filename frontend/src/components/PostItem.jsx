5ed02f60363705a71d2111e6c00f8409fa60cb2d
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2022-04-10 09:38:53 -0500

    Ending the PostItem component

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -1,10 +1,16 @@
 import './styles/PostItem.scss'
 
 const Post = ({post}) => {
+
+    const createdAt = new Date(post.created_at)
+
     return(
         <article className="post-item">
             <h4>{post.title}</h4>
             <p>{post.content}</p>
+            <div className="info">
+                <span>{`${createdAt.toLocaleDateString('es-CO')} ${createdAt.toLocaleTimeString('es-CO')}`}</span>
+            </div>
         </article>
     )
 }
