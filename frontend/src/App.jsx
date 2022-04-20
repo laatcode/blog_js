959bc184b45d3fac13bdb8d02463f8a5c92f1ec1
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2022-04-19 21:43:39 -0500

    Adding footer component and layout improvements for pages in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Home from './pages/Home';
 import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './App.scss'
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Container>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Container>
-    </Router>
+    <div className="App">
+      <Router>
+        <Header />
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './styles/Footer.scss'
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <p>Copyright ® 2022 Laatcode</p>
+        </footer>
+    )
+}
+
+export default Footer
